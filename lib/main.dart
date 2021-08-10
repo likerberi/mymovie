b1f7da36def15a8d3488532343d27eb0dabacdfd
seungyongC <likerberi97@gmail.com>
2021-08-10 14:37:44 +0900

    bug fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 //import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:math_expressions/math_expressions.dart';
-import 'buttons.dart';
+import 'package:flutter_simple_calculator/flutter_simple_calculator.dart';
 import 'package:get/get.dart';
 
 Future<void> main() async{
@@ -15,19 +14,7 @@ class CalcApp extends StatelessWidget {
       fontWeight: FontWeight.w500,
       fontSize: 12);
 
-  final TextStyle selectedLabelStyle =
-  TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 12);
-
-  var userInput = '';
-  var answer = '';
-
-  final List<String> buttons = [
-    'C', '.', '%', 'DEL',
-    '7', '8', '9', '/',
-    '4', '5', '6', 'x',
-    '1', '2', '3', '-',
-    '00', '0', '=', '+',
-  ];
+  final TextStyle selectedLabelStyle = TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 12);
 
   buildBottomNavigationMenu(context, landingPageController) {
     return Obx(() => SizedBox(
@@ -85,6 +72,7 @@ class CalcApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final DashboardController landingPageController = Get.put(DashboardController(), permanent: false);
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: SafeArea(
         child: Scaffold(
           bottomNavigationBar:
@@ -103,173 +91,24 @@ class CalcApp extends StatelessWidget {
 
 class Calculation extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => _Calculation();
+  State<StatefulWidget> createState() {
+    return _CalculationState();
+  }
 }
 
-class _Calculation extends State<Calculation> {
-  var userInput = '';
-  var answer = '';
-
-  // Array of button
-  final List<String> buttons = [
-    'C', '.', '%', 'DEL',
-    '7', '8', '9', '/',
-    '4', '5', '6', 'x',
-    '1', '2', '3', '-',
-    '00', '0', '=', '+',
-  ];
-
+class _CalculationState extends State<Calculation> {
+  double _currentValue = 0.0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: new AppBar(
-        title: new Text("Calculator"),
-      ), //AppBar
-      backgroundColor: Colors.white38,
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: Container(
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.all(20),
-                      alignment: Alignment.centerRight,
-                      child: Text(
-                        userInput,
-                        style: TextStyle(fontSize: 18, color: Colors.black),
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.all(15),
-                      alignment: Alignment.centerRight,
-                      child: Text(
-                        answer,
-                        style: TextStyle(
-                            fontSize: 30,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    )
-                  ]),
-            ),
-          ),
-          Expanded(
-            flex: 7,
-
-            child: Container(
-              child: GridView.builder(
-                  itemCount: buttons.length,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 4),
-                  itemBuilder: (BuildContext context, int index) {
-                    // Clear Button
-                    if (index == 0) {
-                      return Buttons(
-                        buttonTap: () {
-                          setState(() {
-                            userInput = '';
-                            answer = '0';
-                          });
-                        },
-                        buttonText: buttons[index],
-                        color: Colors.blue[50],
-                        textColor: Colors.black,
-                      );
-                    }
-
-                    // +/- button
-                    else if (index == 1) {
-                      return Buttons(
-                        buttonText: buttons[index],
-                        color: Colors.blue[50],
-                        textColor: Colors.black,
-                      );
-                    }
-                    // % Button
-                    else if (index == 2) {
-                      return Buttons(
-                        buttonTap: () {
-                          setState(() {
-                            userInput += buttons[index];
-                          });
-                        },
-                        buttonText: buttons[index],
-                        color: Colors.blue[50],
-                        textColor: Colors.black,
-                      );
-                    }
-                    // Delete Button
-                    else if (index == 3) {
-                      return Buttons(
-                        buttonTap: () {
-                          setState(() {
-                            userInput =
-                                userInput.substring(0, userInput.length - 1);
-                          });
-                        },
-                        buttonText: buttons[index],
-                        color: Colors.blue[50],
-                        textColor: Colors.black,
-                      );
-                    }
-                    // Equal_to Button
-                    else if (index == 18) {
-                      return Buttons(
-                        buttonTap: () {
-                          setState(() {
-                            equalPressed();
-                          });
-                        },
-                        buttonText: buttons[index],
-                        color: Colors.orange[700],
-                        textColor: Colors.white,
-                      );
-                    }
-
-                    //  other buttons
-                    else {
-                      return Buttons(
-                        buttonTap: () {
-                          setState(() {
-                            userInput += buttons[index];
-                          });
-                        },
-                        buttonText: buttons[index],
-                        color: isOperator(buttons[index])
-                            ? Colors.blueAccent
-                            : Colors.white,
-                        textColor: isOperator(buttons[index])
-                            ? Colors.white
-                            : Colors.black,
-                      );
-                    }
-                  }), // GridView.builder
-            ),
-          ),
-        ],
+    return Column(children: <Widget>[
+      Expanded(
+        child: SimpleCalculator(),
       ),
-    );
-  }
-
-  bool isOperator(String x) {
-    if (x == '/' || x == 'x' || x == '-' || x == '+' || x == '=') {
-      return true;
-    }
-    return false;
-  }
-
-// function to calculate the input operation
-  void equalPressed() {
-    String finaluserinput = userInput;
-    finaluserinput = userInput.replaceAll('x', '*');
-
-    Parser p = Parser();
-    Expression exp = p.parse(finaluserinput);
-    ContextModel cm = ContextModel();
-    double eval = exp.evaluate(EvaluationType.REAL, cm);
-    answer = eval.toString();
+      // Expanded(
+      //   child: Simple,
+      //   flex: 5,
+      // ),
+    ]);
   }
 }
 
@@ -282,13 +121,11 @@ class DashboardController extends GetxController {
 
   @override
   void onInit() {
-    // TODO: implement onInit
     super.onInit();
   }
 
   @override
   void dispose() {
-    // TODO: implement dispose
     super.dispose();
   }
 }
